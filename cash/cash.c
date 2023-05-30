aacc264f124c108fff37d5a8c94adac6d4a42760
Clive Sasaka <clivesasaka@gmail.com>
2023-05-30 19:10:24 +0300

    Cash task done

--- cash/cash.c
+++ cash/cash.c
@@ -1,12 +1,13 @@
 // #include <cs50.h>
 #include <stdio.h>
-
+// Prototypes
 int get_cents(void);
 int calculate_quarters(int cents);
 int calculate_dimes(int cents);
 int calculate_nickels(int cents);
 int calculate_pennies(int cents);
 
+// Greedy algorithm
 int main(void)
 {
     // Ask how many cents the customer is owed
@@ -14,20 +15,19 @@ int main(void)
 
     // Calculate the number of quarters to give the customer
     int quarters = calculate_quarters(cents);
-    cents = cents - quarters * 25;
+    cents = cents - quarters * 25; 
 
     // Calculate the number of dimes to give the customer
     int dimes = calculate_dimes(cents);
-    cents = cents - dimes * 10;
+    cents = cents - dimes * 10; 
 
     // Calculate the number of nickels to give the customer
     int nickels = calculate_nickels(cents);
-    cents = cents - nickels * 5;
+    cents = cents - nickels * 5; 
 
     // Calculate the number of pennies to give the customer
     int pennies = calculate_pennies(cents);
-    cents = cents - pennies * 1;
-
+    cents = cents - pennies * 1; 
     // Sum coins
     int coins = quarters + dimes + nickels + pennies;
 
@@ -37,30 +37,37 @@ int main(void)
 
 int get_cents(void)
 {
-    // TODO
-    return 0;
+    int cents;
+    do
+    {
+        printf("Change owed: ");
+        scanf("%i", &cents);
+    }
+    while (cents < 0);
+
+    return cents;
 }
 
 int calculate_quarters(int cents)
 {
-    // TODO
-    return 0;
+    int quarters = cents / 25;
+    return quarters;
 }
 
 int calculate_dimes(int cents)
 {
-    // TODO
-    return 0;
+    int dimes = cents / 10;
+    return dimes;
 }
 
 int calculate_nickels(int cents)
 {
-    // TODO
-    return 0;
+    int nickels = cents / 5;
+    return nickels;
 }
 
 int calculate_pennies(int cents)
 {
-    // TODO
-    return 0;
+    int pennies = cents / 1;
+    return pennies;
 }
