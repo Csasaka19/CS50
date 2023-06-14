3583b2553bb75262188b7ae5f5d804779ac8f7b3
Clive Sasaka <clivesasaka@gmail.com>
2023-06-14 18:52:34 +0300

    Scrabble battle solved

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,5 +1,5 @@
+#include <cs50.h>
 #include <ctype.h>
-// #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -19,9 +19,36 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score2 > score1)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
 }
 
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
+    int score = 0;
+    int word_length = strlen(word);
+    for (int i = 0; i < word_length; i++)
+    {
+        if (isupper(word[i]))
+        {
+            score += POINTS[word[i] - 'A'];
+        }
+        else if (islower(word[i]))
+        {
+            score += POINTS[word[i] - 'a'];
+        }
+    }
+
+    return score;
 }
