4e1a86f3b9163f893129f822248b31214d723477
Clive Sasaka <clivesasaka@gmail.com>
2023-12-09 14:43:55 +0300

    N0 v0w3ls d0n3

--- Week_2/no-vowels/no-vowels.c
+++ Week_2/no-vowels/no-vowels.c
@@ -1,12 +1,47 @@
-// Write a function to replace vowels with numbers
-// Get practice with strings
-// Get practice with command line
-// Get practice with switch
-
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+
+string replace(string a);
 
 int main(int argc, string argv[])
 {
-    
+    if (argc != 2)
+    {
+        printf("Usage: ./no-vowels word\n");
+        return 1;
+    }
+    string word = argv[1];
+    string replaced = replace(word);
+    printf("%s\n", replaced);
+}
+
+string replace(string a)
+{
+    for (int i = 0; i < strlen(a); i++)
+    {
+        switch (a[i])
+        {
+            case 'a':
+                a[i] = '6';
+                break;
+
+            case 'e':
+                a[i] = '3';
+                break;
+
+            case 'i':
+                a[i] = '1';
+                break;
+
+            case 'o':
+                a[i] = '0';
+                break;
+
+            case 'u':
+                a[i] = 'u';
+                break;
+        }
+    }
+    return a;
 }
