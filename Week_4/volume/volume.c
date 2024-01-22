1d305d5d2a4c54f52d097ce6404730a678d8c848
Clive Sasaka <clivesasaka@gmail.com>
2024-01-22 16:50:34 +0300

    Volume problem set complete

--- Week_4/volume/volume.c
+++ Week_4/volume/volume.c
@@ -33,9 +33,23 @@ int main(int argc, char *argv[])
 
     float factor = atof(argv[3]);
 
-    // TODO: Copy header from input file to output file
+    // Copy header from input file to output file
+    uint8_t header[HEADER_SIZE];
+    
+    fread(header, sizeof(uint8_t), HEADER_SIZE, input);
+    fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
 
     // TODO: Read samples from input file and write updated data to output file
+    int16_t buffer;
+
+    while (fread(&buffer, sizeof(int16_t), 1, input))
+    {
+        // Multiply the wav buffer by a predetermined user declared factor
+         buffer *= factor;
+
+        // Write the updated part to output
+        fwrite(&buffer, sizeof(int16_t), 1, output);
+    }
 
     // Close files
     fclose(input);
