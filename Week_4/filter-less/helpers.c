f224cc32eff03bc55606aae8742b412143707ba0
Clive Sasaka <clivesasaka@gmail.com>
2024-01-31 21:03:22 +0300

    Add the implementation of reflection and blurring with some huge help!

--- Week_4/filter-less/helpers.c
+++ Week_4/filter-less/helpers.c
@@ -1,4 +1,7 @@
 #include "helpers.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -59,11 +62,88 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Loop through each pixel in the image
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+           //swap pixels the left should be in the right and the right should be in the left
+           if (width % 2 == 0)
+           {
+               if (j < width / 2)
+               {
+                   RGBTRIPLE temp = image[i][j];
+                   image[i][j] = image[i][width - j - 1];
+                   image[i][width - j - 1] = temp;
+               }
+           }
+           else
+           {
+               if (j <= width / 2)
+               {
+                   RGBTRIPLE temp = image[i][j];
+                   image[i][j] = image[i][width - j - 1];
+                   image[i][width - j - 1] = temp;
+               }
+           }
+        }
+        
+    }
+    
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    // Create a copy of image
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Copy each pixel to the copy array
+            copy[i][j] = image[i][j];
+
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Initialize the sum of red, green, and blue values to 0
+            float sumRed = 0;
+            float sumGreen = 0;
+            float sumBlue = 0;
+            float counter = 0;
+
+            // Loop through each pixel in the image
+            for (int k = -1; k < 2; k++)
+            {
+                // Check if the pixel is out of bounds
+                if (i + k < 0 || i + k > height - 1)
+                {
+                    continue;
+                }
+                for (int l = -1; l < 2; l++)
+                {
+                    // Check if the pixel is out of bounds
+                    if (j + l < 0 || j + l > width - 1)
+                    {
+                        continue;
+                    }
+                    // Add the red, green, and blue values of the pixel to the sum
+                    sumRed += copy[i + k][j + l].rgbtRed;
+                    sumGreen += copy[i + k][j + l].rgbtGreen;
+                    sumBlue += copy[i + k][j + l].rgbtBlue;
+                    counter++;
+                }
+            }
+            // Update the original red, green, and blue values to the new blurred value
+            image[i][j].rgbtRed = round(sumRed / counter);
+            image[i][j].rgbtGreen = round(sumGreen / counter);
+            image[i][j].rgbtBlue = round(sumBlue / counter);
+        }
+    }
 }
